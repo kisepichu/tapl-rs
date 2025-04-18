c984c4bbb0e56e0dc86f80861bba520b208e325f
kise <kisepichu@gmail.com>
2025-04-18 20:12:54 +0900

    setup workspace

---
+++ target/debug/deps/lambda-8493f13b5d9c944a.d
@@ -0,0 +1,9 @@
+/home/kise/repos/tapl-rs/target/debug/deps/liblambda-8493f13b5d9c944a.rmeta: lambda/src/main.rs Cargo.toml
+
+/home/kise/repos/tapl-rs/target/debug/deps/lambda-8493f13b5d9c944a.d: lambda/src/main.rs Cargo.toml
+
+lambda/src/main.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=
+# env-dep:CLIPPY_CONF_DIR
---
+++ target/debug/deps/lambda-cf70a3ffeb25b79c.d
@@ -0,0 +1,9 @@
+/home/kise/repos/tapl-rs/target/debug/deps/liblambda-cf70a3ffeb25b79c.rmeta: lambda/src/main.rs Cargo.toml
+
+/home/kise/repos/tapl-rs/target/debug/deps/lambda-cf70a3ffeb25b79c.d: lambda/src/main.rs Cargo.toml
+
+lambda/src/main.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=
+# env-dep:CLIPPY_CONF_DIR
